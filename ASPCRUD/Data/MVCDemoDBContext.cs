395af3bff03427089f25f43332676946b330c23f
Godsent Ukomadu <Godsentukomadu@outlook.com>
2023-05-13 09:24:26 +0100

    Add project files.

---
+++ ASPCRUD/Data/MVCDemoDBContext.cs
@@ -0,0 +1,14 @@
+﻿using ASPCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPCRUD.Data
+{
+    public class MVCDemoDBContext : DbContext
+    {
+        public MVCDemoDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
